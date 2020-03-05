99ca5cfe57367597ee97a95aeaaff256cc2005a7
Jose Ferando Valdez Perez <valdezpjose@gmail.com>
2020-03-05 17:52:31 -0600

    Adicion de Mensaje de bienvenida menu-201503651

--- Analisis/[AyD1]P1_5/src/ayd1/p1_5/AyD1P1_5.java
+++ Analisis/[AyD1]P1_5/src/ayd1/p1_5/AyD1P1_5.java
@@ -17,7 +17,7 @@ public class AyD1P1_5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-        
+        JOptionPane.showMessageDialog(null, "Analisis Grupo 5 Practica 1 Bienvenido");
         Scanner teclado = new Scanner(System.in);
         String menu;
         menu = teclado.nextLine();
